1fc89f6d93d2c94b06e88e8cad607d1cecccc550
Guo Yingwei <41852697+willgyw@users.noreply.github.com>
2020-06-24 09:09:35 +0800

    Update 444.py

--- 444.py
+++ 444.py
@@ -16,8 +16,8 @@ import time
 
 if __name__ == '__main__':
     #这里改成你的统一认证用户名和密码
-    user_name = '2019050408'
-    pwd = 'Nwafu912001'
+    user_name = '2019055264'
+    pwd = 'wr05517'
 
     # 加上这两句话不打开浏览器
     option = webdriver.ChromeOptions()
